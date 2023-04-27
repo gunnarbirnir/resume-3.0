f0b712c0d56fc27250b15fce586d1803275a0969
Gunnar Birnir Ólafsson <gunnarbirnir@gmail.com>
2023-04-27 15:36:18 +0200

    Background image

--- src/components/elements/Card/index.tsx
+++ src/components/elements/Card/index.tsx
@@ -1,21 +1,30 @@
-import type { FC, PropsWithChildren } from "react";
+import type { FC, PropsWithChildren, CSSProperties } from "react";
 import clsx from "clsx";
 
 import styles from "./styles.module.css";
 
 interface Props {
   scrollable?: boolean;
+  className?: string;
+  style?: CSSProperties;
   onClick?: () => void;
 }
 
 const Card: FC<PropsWithChildren<Props>> = ({
   scrollable = false,
+  className,
+  style,
   children,
   onClick,
 }) => {
   return (
     <div
-      className={clsx(styles.card, { [styles.clickableCard]: onClick })}
+      className={clsx(
+        styles.card,
+        { [styles.clickableCard]: onClick },
+        className
+      )}
+      style={style}
       onClick={onClick}
     >
       <div style={{ height: scrollable ? "auto" : "100%" }}>{children}</div>
