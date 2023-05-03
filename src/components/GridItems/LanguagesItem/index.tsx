936cb936a9f3f454452f3bd54473d00a61bc77af
Gunnar Birnir Ólafsson <gunnarbirnir@gmail.com>
2023-05-03 16:49:12 +0200

    Language item

--- src/components/GridItems/LanguagesItem/index.tsx
+++ src/components/GridItems/LanguagesItem/index.tsx
@@ -1,12 +1,31 @@
 import type { FC } from "react";
+import { Fragment } from "react";
 
+import languages from "../../../assets/json/languages.json";
 import { Card } from "../../elements";
 import type { GridItemProps } from "../types";
+import styles from "./styles.module.css";
 
 const LanguagesItem: FC<GridItemProps> = ({ inTransition }) => {
   return (
     <Card inTransition={inTransition}>
-      <h2>Languages</h2>
+      <div className={styles.languageItem}>
+        {languages.map((language, index) => (
+          <Fragment key={language.code}>
+            <div className={styles.langContainer}>
+              <p className={styles.langCode}>{language.code}</p>
+              <div>
+                <p className={styles.langTitle}>{language.title}</p>
+                <p className={styles.langLevel}>{language.level}</p>
+              </div>
+            </div>
+
+            {index !== languages.length - 1 && (
+              <div className={styles.langDivider} />
+            )}
+          </Fragment>
+        ))}
+      </div>
     </Card>
   );
 };
