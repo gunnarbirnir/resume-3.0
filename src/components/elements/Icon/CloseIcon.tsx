33e461f603ea21c1e6b6685974b6b8e22a4ab173
Gunnar Birnir Ólafsson <gunnarbirnir@gmail.com>
2023-05-07 01:36:14 +0200

    Safari fixes

--- src/components/elements/Icon/CloseIcon.tsx
+++ src/components/elements/Icon/CloseIcon.tsx
@@ -7,7 +7,7 @@ const CloseIcon: FC = () => {
       width="24"
       height="24"
       viewBox="0 0 24 24"
-      fill="none"
+      // fill="none"
       stroke="currentColor"
       strokeWidth="2"
       strokeLinecap="round"
