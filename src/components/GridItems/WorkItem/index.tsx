b83db412fbca4de74170043958fe603f3e28cd5f
Gunnar Birnir Ólafsson <gunnarbirnir@gmail.com>
2023-05-09 12:28:47 +0200

    Clean up work section

--- src/components/GridItems/WorkItem/index.tsx
+++ src/components/GridItems/WorkItem/index.tsx
@@ -4,10 +4,15 @@ import { motion } from "framer-motion";
 import clsx from "clsx";
 
 import work from "../../../assets/json/work.json";
-// import { FADE_IN_DURATION_SEC } from "../../../constants";
+import { FADE_IN_DURATION_SEC } from "../../../constants";
 import { Card } from "../../elements";
 import type { GridActionItemProps } from "../types";
 import styles from "./styles.module.css";
+
+const ANIMATION_DURATION = 0.5;
+const CIRCLE_INIT_SIZE = 12;
+const CIRCLE_SIZE = 26;
+const CIRCLE_BORDER_WIDTH = 4;
 
 const WorkItem: FC<GridActionItemProps> = ({
   active,
@@ -15,6 +20,70 @@ const WorkItem: FC<GridActionItemProps> = ({
   setActive,
 }) => {
   const [hoverLinkIndex, setHoverLinkIndex] = useState(-1);
+
+  const renderJob = (job: any, index: number) => {
+    const animationDelay = FADE_IN_DURATION_SEC * (index + 1);
+    const linkProps = {
+      href: job.link,
+      target: "_blank",
+      rel: "noreferrer",
+      onMouseEnter: () => setHoverLinkIndex(index),
+      onMouseLeave: () => setHoverLinkIndex(-1),
+    };
+
+    return (
+      <div
+        key={job.company}
+        className={clsx(styles.jobContainer, {
+          [styles.hoveringLink]: hoverLinkIndex === index,
+        })}
+      >
+        <div className={styles.jobCircleContainer}>
+          <a {...linkProps}>
+            <motion.div
+              initial={{
+                height: CIRCLE_INIT_SIZE,
+                width: CIRCLE_INIT_SIZE,
+                borderWidth: CIRCLE_INIT_SIZE / 2,
+              }}
+              animate={{
+                height: CIRCLE_SIZE,
+                width: CIRCLE_SIZE,
+                borderWidth: CIRCLE_BORDER_WIDTH,
+              }}
+              transition={{
+                type: "spring",
+                delay: animationDelay,
+                duration: ANIMATION_DURATION,
+                iterations: 1,
+              }}
+              className={styles.jobCircle}
+            />
+          </a>
+        </div>
+        <motion.div
+          className="f-1"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{
+            delay: animationDelay,
+            duration: ANIMATION_DURATION,
+          }}
+        >
+          <h3 className={styles.jobCompany}>
+            <a {...linkProps}>{job.company}</a>
+          </h3>
+          <p className={styles.jobTitle}>
+            {job.title}
+            <span className={styles.jobYears}>
+              {job.start === job.end ? job.start : `${job.start} - ${job.end}`}
+            </span>
+          </p>
+          <p className={styles.jobDescription}>{job.description}</p>
+        </motion.div>
+      </div>
+    );
+  };
 
   return (
     <Card
@@ -24,67 +93,7 @@ const WorkItem: FC<GridActionItemProps> = ({
       expanded={active}
       setExpanded={setActive}
     >
-      <div className={styles.workItem}>
-        {work.jobs.map((job, index) => (
-          <div
-            key={job.company}
-            className={clsx(styles.jobContainer, {
-              [styles.hoveringLink]: hoverLinkIndex === index,
-            })}
-          >
-            <div className={styles.jobCircleContainer}>
-              <a
-                href={job.link}
-                target="_blank"
-                rel="noreferrer"
-                onMouseEnter={() => setHoverLinkIndex(index)}
-                onMouseLeave={() => setHoverLinkIndex(-1)}
-              >
-                <motion.div
-                  initial={{ height: 12, width: 12, borderWidth: 6 }}
-                  animate={{ height: 25, width: 25, borderWidth: 4 }}
-                  transition={{
-                    type: "spring",
-                    delay: 0.3 * (index + 1),
-                    duration: 0.5,
-                  }}
-                  className={styles.jobCircle}
-                />
-              </a>
-            </div>
-            <motion.div
-              className="f-1"
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1 }}
-              transition={{
-                delay: 0.3 * (index + 1),
-                duration: 0.5,
-              }}
-            >
-              <h3 className={styles.jobCompany}>
-                <a
-                  href={job.link}
-                  target="_blank"
-                  rel="noreferrer"
-                  onMouseEnter={() => setHoverLinkIndex(index)}
-                  onMouseLeave={() => setHoverLinkIndex(-1)}
-                >
-                  {job.company}
-                </a>
-              </h3>
-              <p className={styles.jobTitle}>
-                {job.title}
-                <span className={styles.jobYears}>
-                  {job.start === job.end
-                    ? job.start
-                    : `${job.start} - ${job.end}`}
-                </span>
-              </p>
-              <p className={styles.jobDescription}>{job.description}</p>
-            </motion.div>
-          </div>
-        ))}
-      </div>
+      <div className={styles.workItem}>{work.jobs.map(renderJob)}</div>
     </Card>
   );
 };
