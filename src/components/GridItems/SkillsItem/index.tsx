7359f3282c941375dfd2fef613157223d15060f7
Gunnar Birnir Ólafsson <gunnarbirnir@gmail.com>
2023-05-15 14:59:27 +0200

    Skill item layout

--- src/components/GridItems/SkillsItem/index.tsx
+++ src/components/GridItems/SkillsItem/index.tsx
@@ -1,15 +1,40 @@
 import type { FC } from "react";
+import { motion } from "framer-motion";
 
 import skills from "../../../assets/json/skills.json";
 import Card from "../../Card";
 import type { GridActionItemProps } from "../types";
 import styles from "./styles.module.css";
 
+const ANIMATION_DELAY = 0.05;
+const ANIMATION_DURATION = 0.2;
+const ANIMATION_GROUP_SIZE = 3;
+
 const SkillsItem: FC<GridActionItemProps> = ({
   active,
   inTransition,
   setActive,
 }) => {
+  const renderSkill = (skill: string, index: number) => {
+    const animationDelay =
+      Math.floor(index / ANIMATION_GROUP_SIZE) * ANIMATION_DELAY;
+
+    return (
+      <motion.p
+        key={skill}
+        className={styles.skill}
+        initial={{ opacity: 0, transform: "translateX(10px)" }}
+        animate={{ opacity: 1, transform: "translateX(0px)" }}
+        transition={{
+          delay: animationDelay,
+          duration: ANIMATION_DURATION,
+        }}
+      >
+        {skill}
+      </motion.p>
+    );
+  };
+
   return (
     <Card
       scrollable
@@ -19,18 +44,14 @@ const SkillsItem: FC<GridActionItemProps> = ({
       setExpanded={setActive}
     >
       <div className={styles.skillsItem}>
-        {skills.general.map((skill) => (
-          <p key={skill} className={styles.skill}>
-            {skill}
-          </p>
-        ))}
-        <div className={styles.tools}>
-          <h3>{skills.toolsTitle}</h3>
-          {skills.tools.map((skill) => (
-            <p key={skill} className={styles.skill}>
-              {skill}
-            </p>
-          ))}
+        <h3 className={styles.generalTitle}>{skills.generalTitle}</h3>
+        <div className={styles.skillsContainer}>
+          {skills.general.map(renderSkill)}
+        </div>
+
+        <h3>{skills.toolsTitle}</h3>
+        <div className={styles.skillsContainer}>
+          {skills.tools.map(renderSkill)}
         </div>
       </div>
     </Card>
