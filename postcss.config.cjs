a3cbb507489c46921fffe4905517048d5f1d73a6
Gunnar Birnir Ólafsson <gunnarbirnir@gmail.com>
2023-05-19 22:42:52 +0200

    Media query variables

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,3 +1,10 @@
 module.exports = {
-  plugins: [require("autoprefixer"), require("cssnano")],
+  plugins: [
+    require("autoprefixer"),
+    require("cssnano"),
+    require("@csstools/postcss-global-data")({
+      files: ["src/styles/media-queries.css"],
+    }),
+    require("postcss-custom-media"),
+  ],
 };
