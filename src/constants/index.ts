18bd0f5d943687f17775b19c78661559995f961d
Gunnar Birnir Ólafsson <gunnarbirnir@gmail.com>
2023-05-20 20:08:27 +0200

    Refactor grid styling

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -9,8 +9,11 @@ export const FADE_IN_DURATION_SEC = msToSec(FADE_IN_DURATION_MS);
 
 // Keep in sync with media-queries.css
 export const MEDIA_QUERY = {
+  // 845 is the lowest possible, add 100
   LARGE_DESKTOP_HEIGHT: 945,
+  // A small buffer from tablet breakpoint
   LARGE_DESKTOP_WIDTH: 1250,
+  // Grid max width plus padding
   TABLET: 1200,
   MOBILE: 700,
 };
