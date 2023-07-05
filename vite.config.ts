5ee7dc0a1b4f78a7317bac63d7eb18aa22fc36df
Gunnar Birnir Ólafsson <gunnarbirnir@gmail.com>
2023-07-05 13:05:47 +0200

    PWA config

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,8 @@ import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), VitePWA()],
+  plugins: [
+    react(),
+    VitePWA({ workbox: { navigateFallbackDenylist: [/resume\.pdf/] } }),
+  ],
 });
